8caa598ad6fad0139c7776916c256ac46928edaf
unknown <sumairbinkalam20@gmail.com>
2022-11-10 13:59:53 +0600

    footer section added

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <div>
+            <div className='contianer'>
+                <div className='row'>
+                    <div className='py-5 bg-dark'>
+                        <Link className='navbar-brand' to='/'><h3 className='text-white text-center'>Care-dent</h3></Link>
+                        
+                        <p className='text-white text-center'>
+                        Copyright © 2022 Md. Sumayer
+                        </p>
+                    </div>
+                </div>
+            </div>
            
         </div>
     );
